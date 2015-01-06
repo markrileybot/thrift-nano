5d5d33a862c08ed93ebdeb20a8111a9eb512119b
Mark Riley <mark@eucleo.com>
2015-01-06 03:30:12 -0500

    Need to be a little more flexible for some systems

--- lib/c_nano/src/thrift-nano/init.c
+++ lib/c_nano/src/thrift-nano/init.c
@@ -2,7 +2,11 @@
 #include <thrift-nano/init.h>
 #include <thrift-nano/mem.h>
 
+#ifdef TN_OS_UNKNOWN
+void tn_init(void)
+#else
 TN_BOOTSTRAP_FUNC(tn_init)
+#endif
 {
     // init allocator
     tn_alloc_init();
