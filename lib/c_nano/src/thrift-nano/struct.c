9b382f6c3b97025ee2afb2e91eb3abd8c78834ec
Mark Riley <mark@eucleo.com>
2015-05-04 17:36:27 -0400

    no auto-reset here...this can cause trouble

--- lib/c_nano/src/thrift-nano/struct.c
+++ lib/c_nano/src/thrift-nano/struct.c
@@ -7,7 +7,6 @@ tn_struct_write(void *data, tn_protocol_t *protocol, tn_transport_t *transport, 
 {
 	tn_struct_t *s = (tn_struct_t*) data;
 	size_t len = s->tn_write(data, protocol, transport, error);
-	tn_object_reset(protocol);
 	return len;
 }
 
@@ -16,7 +15,6 @@ tn_struct_read(void *data, tn_protocol_t *protocol, tn_transport_t *transport, t
 {
 	tn_struct_t *s = (tn_struct_t*) data;
 	size_t len = s->tn_read(data, protocol, transport, error);
-	tn_object_reset(protocol);
 	return len;
 }
 
